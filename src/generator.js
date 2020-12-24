2eca4bbefdf0b8b7f95df5f579f465ece749bb2d
DanMat <dannymatthew@gmail.com>
2020-12-23 19:15:36 -0500

    Intall depedencies inside the template

--- src/generator.js
+++ src/generator.js
@@ -29,10 +29,15 @@ module.exports = class extends Generator {
 		});
 
 		// Copy all dotfiles
-		this.fs.copy(this.templatePath(pathToTemplate), this.destinationRoot(), {}, {}, {globOptions: {ignore:".git"}});
+		this.fs.copy(this.templatePath(pathToTemplate + '/.*'), this.destinationRoot(), {}, {}, {globOptions: {ignore:".git"}});
 
 		this.fs.move(this.destinationPath('_gitignore'), this.destinationPath('.gitignore'));
 		this.fs.move(this.destinationPath('_package.json'), this.destinationPath('package.json'));
+
+		this.installDependencies({
+			bower: true,
+			npm: true
+		});
 
 	}
 
