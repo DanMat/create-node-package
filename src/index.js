aca1347da3633f093143923313dc8d057f3845e2
DanMat <dannymatthew@gmail.com>
2020-12-26 17:05:02 -0500

    Using command to short circuit auto npm token logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import figlet from 'figlet';
+import { Command } from 'commander';
+
 import prerequisite from './steps/prerequisite';
 import getUsersNPMToken from './steps/npmtoken';
 
 const LOGO = 'CREATE DEPLOY NODE PACKAGES';
 const FONT_STYLE = 'contessa';
 
-export const runCLI = async _args => {
+export const runCLI = async args => {
 	console.log(figlet.textSync(LOGO, FONT_STYLE));
 
+	const command = new Command();
+	command.option(
+		'-n, --npm_token <token>',
+		'Create a NPM token using this doc https://docs.npmjs.com/creating-and-viewing-access-tokens'
+	);
+	command.parse(args);
+
+	let { npm_token: npmToken } = command.opts();
+
 	await prerequisite.run();
-	await getUsersNPMToken();
+
+	if (!npmToken) {
+		npmToken = await getUsersNPMToken();
+	}
+
+	console.log(npmToken);
 };
